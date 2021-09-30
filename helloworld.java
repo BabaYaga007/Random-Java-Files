013b8d4ce4dad05a5982f86c5e5b54b6048d3343
achint769 <32853198+achint769@users.noreply.github.com>
2021-10-01 03:37:44 +0530

    Added a simple java program
    Hello World program

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+// Program to create simple hello program in Java 
+
+    class HelloWorld{  
+        public static void main(String args[]){  
+          
+         System.out.println("Hello World");
+         System.out.println("Welcome to the world of coding"):
+        }  
+    }  
